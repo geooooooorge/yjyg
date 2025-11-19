7bb2480f361c9732c497355cb4f13ad2c074e5a4
geooooooorge <geooooooorge@users.noreply.github.com>
2025-11-19 16:00:15 +0800

    Add rewrites for Hundsun API proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/hundsun',
+        destination: 'https://hundsun-api-app.vercel.app',
+      },
+      {
+        source: '/hundsun/:path*',
+        destination: 'https://hundsun-api-app.vercel.app/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
